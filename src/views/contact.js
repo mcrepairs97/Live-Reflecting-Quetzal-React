75063f5faa6bc379a20efaa39c4e05814155f420
Martins Computer Repairs <93351588+mcrepairs97@users.noreply.github.com>
2023-10-26 19:49:22 +0200

    Commit from TeleportHQ

--- src/views/contact.js
+++ src/views/contact.js
@@ -184,10 +184,11 @@ const Contact = (props) => {
       </div>
       <div className="contact-separator1"></div>
       <div className="contact-container03">
-        <div>
+        <div className="contact-container04">
           <div className="contact-container05">
             <Script
               html={`<form name="contact" method="POST" data-netlify="true">
+  <input type="hidden" name="form-name" value="contact" />
   <p>
     <label>Your Name: <input type="text" name="name" /></label>
   </p>
